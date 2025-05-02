928b6caf7488677313e0a518669b99f9969f8a3b
Benedikt Zundel <benedikt.zundel@live.com>
2025-05-02 02:25:50 +0200

    feat: add database seed script for initial ik

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,6 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     OtterWebsite.Repo.insert!(%OtterWebsite.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias OtterWebsite.Repo
+alias OtterWebsite.Accounts.InvitationKey
+
+unless Repo.exists?(InvitationKey) do
+  Repo.insert!(%InvitationKey{key: "INITIAL_ADMIN_KEY"})
+end
